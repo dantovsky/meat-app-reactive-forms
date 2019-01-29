0fb0a55a2c101ef0df8dd570cb25755b8096a845
Dante Marinho <dante-marinho@load-interactive.com>
2019-01-29 22:55:32 +0000

    # Aula 79 - Módulos em Angular, carregamento tardio (Lazy Loading), módulo compartilhado, feature lodule e criação de um Core Module com Serviços (este módulo será apagado na aula seguinte, estando no lugar a Adição de Serviços a um Módulo Compartilhado)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,31 +9,33 @@ import { ROUTES } from './app.routes'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
-import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
-import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
-import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
+import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from './core/core.module'
+// import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
+// import { OrderComponent } from './order/order.component';
+// import { OrderItemsComponent } from './order/order-items/order-items.component';
+// import { InputComponent } from './shared/input/input.component';
+// import { RadioComponent } from './shared/radio/radio.component';
+// import { OrderService } from './order/order.service';
+// import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
+// import { AboutComponent } from './about/about.component'; // foi retirado tambem da lista de declaracoes
+// import { RestaurantsService } from './restaurants/restaurants.service';
+// import { RatingComponent } from './shared/rating/rating.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
@@ -41,22 +43,16 @@ import { RatingComponent } from './shared/rating/rating.component';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    InputComponent,
-    RadioComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
     OrderSummaryComponent,
-    RatingComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule,
-    ReactiveFormsModule,
+    SharedModule,
+    CoreModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService, ShoppingCartService, OrderService, { provide: LOCALE_ID, useValue: 'pt-BR' }],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
